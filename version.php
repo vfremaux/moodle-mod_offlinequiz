2211e26ff39b14e2d4f37d50b92d99626821637c
valery <valery.fremaux@gmail.com>
2015-11-06 14:33:52 +0100

    Commit fixed version for 29

---
+++ version.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of mod_offlinequiz for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Defines the version of offlinequiz
+ *
+ * This code fragment is called by moodle_needs_upgrading() and
+ * /admin/index.php
+ *
+ * @package       mod_offlinequiz
+ * @author        Juergen Zimmer <zimmerj7@univie.ac.at> <zimmerj7@univie.ac.at>
+ * @copyright     2014 Academic Moodle Cooperation {@link http://www.academic-moodle-cooperation.org}
+ * @since         Moodle 2.2
+ * @license       http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$module->version  = 2015060902;
+$module->release   = "2015-06-16";      // User-friendly version number.
+$module->maturity  = MATURITY_STABLE;
+$module->requires = 2014111000;         // Requires this Moodle version.
+$module->cron     = 3600;               // Period for cron to check this module (secs).
+$module->component = 'mod_offlinequiz'; // Full name of the plugin (used for diagnostics).
